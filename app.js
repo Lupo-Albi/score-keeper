66edfebd58d73835e65f74ea9c6dff87c59aa575
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-04-13 20:24:25 -0300

    Changed code, but it's working just as before. Added new variables for better readability

--- app.js
+++ app.js
@@ -1,44 +1,55 @@
-let scorePlayer1 = document.querySelector('#scoreP1');
-let scorePlayer2 = document.querySelector('#scoreP2');
 const player1Button = document.querySelector('#player1');
 const player2Button = document.querySelector('#player2');
+const displayPlayer1 = document.querySelector('#scoreP1');
+const displayPlayer2 = document.querySelector('#scoreP2');
 const resetButton = document.querySelector('#reset');
 const scoreLimit = document.querySelector('#score-limit');
 
-// Changing the score limit in the select
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+let isGameOver = false;
+
 let maxPoints = parseInt(scoreLimit.value, 10); // Initial value
-scoreLimit.addEventListener('change', function(event){
-    maxPoints = parseInt(event.target.value, 10);
+// Changing the score limit in the select
+scoreLimit.addEventListener('change', function(){
+    maxPoints = parseInt(this.value, 10);
+    reset();
 });
 
-
 player1Button.addEventListener('click', function(){
-    scorePlayer1.innerText = parseInt(scorePlayer1.innerText, 10) + 1;
-
-    checkGame(scorePlayer1, scorePlayer2, maxPoints);
+    if(!isGameOver) {
+        scorePlayer1 += 1;
+        if (scorePlayer1 === parseInt(scoreLimit.value, 10)){
+            isGameOver = true;
+            displayPlayer1.classList.add('winner');
+            displayPlayer2.classList.add('loser');
+        }
+        displayPlayer1.textContent = scorePlayer1;
+    }
 });
 
 player2Button.addEventListener('click', function(){
-    scorePlayer2.innerText = parseInt(scorePlayer2.innerText, 10) + 1;
-
-    checkGame(scorePlayer2, scorePlayer1, maxPoints);
+    if(!isGameOver) {
+        scorePlayer2 += 1;
+        if (scorePlayer2 === parseInt(scoreLimit.value, 10)){
+            isGameOver = true;
+            displayPlayer2.classList.add('winner');
+            displayPlayer1.classList.add('loser');
+        }
+        displayPlayer2.textContent = scorePlayer2;
+    }
 });
 
-resetButton.addEventListener('click', function() {
-    scorePlayer1.innerText = '0';
-    scorePlayer2.innerText = '0';
+resetButton.addEventListener('click', reset);
+
+function reset() {
+    isGameOver = false;
+    scorePlayer1 = 0;
+    scorePlayer2 = 0;
     player1Button.disabled = false;
     player2Button.disabled = false;
-    scorePlayer1.style.color = 'black';
-    scorePlayer2.style.color = 'black';
-});
-
-function checkGame(scorePlayerWin, scorePlayerLose, maxPoints) {
-    if(parseInt(scorePlayerWin.innerText, 10) === maxPoints) {
-        player1Button.disabled = true;
-        player2Button.disabled = true;
-    
-        scorePlayerWin.style.color = 'green';
-        scorePlayerLose.style.color = 'red';
-    }
+    displayPlayer1.textContent = scorePlayer1;
+    displayPlayer2.textContent = scorePlayer2
+    displayPlayer1.classList.remove('winner', 'loser');
+    displayPlayer2.classList.remove('winner', 'loser');
 }
